297154f341126a7cce99bf565d793970e8d55937
mmm <proweb2008.m@gmail.com>
2023-11-25 22:03:55 +0500

    changed

--- script.js
+++ script.js
@@ -7,8 +7,7 @@
 
 // const eyes = document.querySelectorAll('.eye .black ');
 // window.addEventListener(`mousemove`, function move(e) {
-//     eyes.forEach(eye => {
-
+//     eyes.forEach(eye => {alias "-myjump" "-jump"
 //         let y = e.pageY + eye.offsetLeft
 //         let x = e.pageX + eye.offsetRight
 //         eye.style.transform = `translate(${x * 10}px, ${y * 10}px)`;
@@ -17,14 +16,19 @@
 // })
 
 
-const eyes = document.querySelectorAll('.eye .black');
+const eyes = document.querySelectorAll('.black');
+
 window.addEventListener('mousemove', function move(e) {
     eyes.forEach(eye => {
-        let y = e.pageY + eye.offsetTop;
-        let x = e.pageX + eye.offsetLeft;
-        eye.style.transform = `translate(${x * 0.1}px, ${y * 0.1}px)`;
+        let speed = eye.getAttribute(`speed`)
+        console.log(speed);
+        let x = (window.innerWidth - e.pageX * speed) / 60;
+        let y = (window.innerHeight - e.pageY * speed) / 60;
+
+        eye.style.transform = `translate(${-x}px, ${-y}px)`;
+        console.log((e.pageX / 2)-  e.pageX);
     });
-}); 
+});
 // console.log(mouth.offsetRight);
 
 // console.log(eye);
